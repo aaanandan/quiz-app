9547d0e666e944f541eccfc38efd9f2e5a988436
anandan balasupramanian <anandan.bs@gmail.com>
2023-03-13 13:14:21 +0530

    updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,4 +3,4 @@ export { default as COUNTDOWN_TIME } from './countdownTime';
 export { default as DIFFICULTY } from './difficulty';
 export { default as NUM_OF_QUESTIONS } from './numOfQuestions';
 export { default as QUESTIONS_TYPE } from './questionsType';
-export const API_URL = 'http://localhost:5050'; //'https://kerserver.onrender.com'; 
+export const API_URL = 'https://kerserver.onrender.com';  //'http://localhost:5050';
